07010641dad83572a1604e64a481159a8a73adc9
double <2637309949@qq.com>
2019-07-02 22:38:35 +0800

    feat: 架构调整

--- application.go
+++ application.go
@@ -15,7 +15,6 @@ import (
 
 // Upload file plugin
 type Upload struct {
-	bulrush.PNBase
 	Path            string
 	PublicURLPrefix string
 	URLPrefix       string
@@ -33,41 +32,39 @@ func New() *Upload {
 }
 
 // Plugin for bulrush
-func (upload *Upload) Plugin() interface{} {
-	return func(router *gin.RouterGroup) {
-		router.POST(upload.URLPrefix, func(c *gin.Context) {
-			form, err := c.MultipartForm()
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"message": err.Error(),
-				})
-				c.Abort()
-			}
-			ret := make([]map[string]interface{}, 0)
-			for _, files := range form.File {
-				for _, file := range files {
-					filename := filepath.Base(file.Filename)
-					uuid := bulrush.RandString(32)
-					uuidFileName := bulrush.RandString(32) + string(filename[len(filename)-len(filepath.Ext(filename)):])
-					if err := c.SaveUploadedFile(file, path.Join(upload.Path, uuidFileName)); err != nil {
-						c.JSON(http.StatusInternalServerError, gin.H{
-							"message": err.Error(),
-						})
-						return
-					}
-					item := map[string]interface{}{
-						"uid":    uuid,
-						"status": "done",
-						"name":   filename,
-						"url":    upload.PublicURLPrefix + "/" + uuidFileName,
-					}
-					ret = append(ret, item)
+func (upload *Upload) Plugin(router *gin.RouterGroup) {
+	router.POST(upload.URLPrefix, func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			c.Abort()
+		}
+		ret := make([]map[string]interface{}, 0)
+		for _, files := range form.File {
+			for _, file := range files {
+				filename := filepath.Base(file.Filename)
+				uuid := bulrush.RandString(32)
+				uuidFileName := bulrush.RandString(32) + string(filename[len(filename)-len(filepath.Ext(filename)):])
+				if err := c.SaveUploadedFile(file, path.Join(upload.Path, uuidFileName)); err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{
+						"message": err.Error(),
+					})
+					return
 				}
+				item := map[string]interface{}{
+					"uid":    uuid,
+					"status": "done",
+					"name":   filename,
+					"url":    upload.PublicURLPrefix + "/" + uuidFileName,
+				}
+				ret = append(ret, item)
 			}
-			c.JSON(http.StatusOK, ret)
-			if upload.Save != nil {
-				upload.Save(c, ret)
-			}
-		})
-	}
+		}
+		c.JSON(http.StatusOK, ret)
+		if upload.Save != nil {
+			upload.Save(c, ret)
+		}
+	})
 }
