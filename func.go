07a64c8c44bb47549ac4f834ecf479adca79927a
double <2637309949@qq.com>
2019-07-15 11:58:19 +0800

    feat: Chain优化

--- func.go
+++ func.go
@@ -26,10 +26,16 @@ func Until(flow ...interface{}) interface{} {
 
 // Chain defined flow (ret, err) chain
 func Chain(flow ...func(interface{}) (interface{}, error)) (interface{}, error) {
-	var result interface{} = 0
+	var result interface{}
 	for index := 0; index < len(flow); index++ {
 		fw := flow[index]
-		ret := reflect.ValueOf(fw).Call([]reflect.Value{reflect.ValueOf(result)})
+		var resultValue reflect.Value
+		if result == nil {
+			resultValue = reflect.Zero(reflect.TypeOf(0))
+		} else {
+			resultValue = reflect.ValueOf(result)
+		}
+		ret := reflect.ValueOf(fw).Call([]reflect.Value{resultValue})
 		result = ret[0].Interface()
 		err := ret[1].Interface()
 		if err != nil {
