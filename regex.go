7f23f85d8694174b0ee4bc67403ffddbba138ca8
double <2637309949@qq.com>
2019-07-19 09:49:03 +0800

    feat: add FindStringSubmatch

---
+++ regex.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2018-2020 Double All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package utils
+
+import "regexp"
+
+// FindStringSubmatch defined regex match sub string
+func FindStringSubmatch(matcher string, s string) []string {
+	var rgx = regexp.MustCompile(matcher)
+	rs := rgx.FindStringSubmatch(s)
+	if rs != nil {
+		return rs[1:]
+	}
+	return []string{}
+}
