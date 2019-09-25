09bbb6ae7afb9a843c7ae6566827b142e2cac5c0
2637309949@qq.com <2637309949@qq.com>
2019-09-26 00:11:04 +0800

    feat: RoutingPoolWithCancel支持初始化并发

--- pool/cancel.go
+++ pool/cancel.go
@@ -14,27 +14,39 @@ import (
 func RoutingPoolWithCancel(worker func(context.CancelFunc), max int64) (context.CancelFunc, chan struct{}) {
 	root := context.Background()
 	ctx, cancel := context.WithCancel(root)
-	currMax := max
+	limit := max
 	curr := int64(0)
 	timer := time.NewTicker(1 * time.Second)
 	done := make(chan struct{}, 0)
+	init := false
+	runWork := func(curr *int64, limit *int64) {
+		if *curr < *limit {
+			func() {
+				defer func() {
+					if *curr > 0 {
+						atomic.AddInt64(curr, -1)
+					}
+				}()
+				atomic.AddInt64(curr, 1)
+				worker(cancel)
+			}()
+		}
+	}
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
-				currMax = 0
+				limit = 0
 				done <- struct{}{}
+				break
 			case <-timer.C:
-				if curr < currMax {
-					go func() {
-						defer func() {
-							if curr > 0 {
-								atomic.AddInt64(&curr, -1)
-							}
-						}()
-						atomic.AddInt64(&curr, 1)
-						worker(cancel)
-					}()
+				if !init {
+					init = true
+					for i := int64(0); i < limit; i++ {
+						go runWork(&curr, &limit)
+					}
+				} else {
+					go runWork(&curr, &limit)
 				}
 			}
 		}
